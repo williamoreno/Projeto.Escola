cb7725c320cd4a3564a6c5a11669c3e4d3d99db3
will <williammorenodtd@gmail.com>
2019-09-12 21:59:47 -0300

    Criando aluno

---
+++ escola/src/escola/Aluno.java
@@ -0,0 +1,11 @@
+package escola;
+
+public class Aluno {
+	String  nome;
+	int idade;
+	double nota1;
+	double nota2;
+	
+	
+
+}
---
+++ escola/src/escola/CriarAluno.java
@@ -0,0 +1,17 @@
+package escola;
+
+public class CriarAluno {
+
+	public static void main(String[] args) {
+		Aluno Aluno1 = new Aluno();
+		Aluno1.nome = "William";
+		Aluno1.idade = 21;
+		Aluno1.nota1 = 10;
+		Aluno1.nota2 = 9.5;
+		
+
+		System.out.println(" O NOME DO ALUNO " + Aluno1.nome);
+
+	}
+
+}
