137084384ae272b599f99f013bcabd2889a222c2
will <williammorenodtd@gmail.com>
2019-09-13 21:57:26 -0300

    comandos de criação e media

--- escola/src/escola/CriarAluno.java
+++ escola/src/escola/CriarAluno.java
@@ -8,10 +8,36 @@ public class CriarAluno {
 		Aluno1.idade = 21;
 		Aluno1.nota1 = 10;
 		Aluno1.nota2 = 9.5;
+		double media1 = (Aluno1.nota1 + Aluno1.nota2) /2 ;
 		
+		Aluno Aluno2 = new Aluno();
+		Aluno2.nome = "vitor";
+		Aluno2.idade = 21;
+		Aluno2.nota1 = 3.4;
+		Aluno2.nota2 = 9.5; 
+		double media2 = (Aluno2.nota1 + Aluno2.nota2) /2 ;
+		
+		System.out.println(" PRIMEIRO ALUNO : "  );
+		System.out.println(" Nome:"  + Aluno1.nome);
+		System.out.println(" idade " + Aluno1.idade);
+		System.out.println(" nota 1" + Aluno1.nota1);
+		System.out.println(" nota 2 " + Aluno1.nota2);
+		System.out.println( " media " + media1);
+		
+		System.out.println();
+	
 
-		System.out.println(" O NOME DO ALUNO " + Aluno1.nome);
-
+		System.out.println(" SEGUNDO  ALUNO : "  );
+		System.out.println(" Nome:"  + Aluno2.nome);
+		System.out.println(" idade " + Aluno2.idade);
+		System.out.println(" nota 1" + Aluno2.nota1);
+		System.out.println(" nota 2 " + Aluno2.nota2);
+		System.out.println( " media " + media2);
+		
+		
+		
+	
+	
+	
 	}
-
 }
